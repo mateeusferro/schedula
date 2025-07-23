e449723b3be37d72999213141285242d7e30fb29
mateus <mateus.03fernandesaraujo@gmail.com>
2025-07-23 19:58:46 -0300

    feat: configure repository and usecase

--- internal/repository/task_repository.go
+++ internal/repository/task_repository.go
@@ -1,1 +1,11 @@
 package repository
+
+import "database/sql"
+
+type TaskRepository struct {
+	DB *sql.DB
+}
+
+func NewTaskRepository(db *sql.DB) *TaskRepository {
+	return &TaskRepository{DB: db}
+}
--- internal/usecase/task_usecase.go
+++ internal/usecase/task_usecase.go
@@ -1,1 +1,13 @@
 package usecase
+
+import (
+	"github.com/mateeusferro/schedula/internal/repository"
+)
+
+type TaskUseCase struct {
+	repo *repository.TaskRepository
+}
+
+func NewTaskUseCase(repo *repository.TaskRepository) *TaskUseCase {
+	return &TaskUseCase{repo: repo}
+}
