638fc10e796b604ef803ed2b3b1d00d51c751857
mateus <mateus.03fernandesaraujo@gmail.com>
2025-07-24 21:29:02 -0300

    feat: create usecase to communicate the server

--- internal/usecase/task_usecase.go
+++ internal/usecase/task_usecase.go
@@ -1,6 +1,7 @@
 package usecase
 
 import (
+	"github.com/mateeusferro/schedula/internal/domain"
 	"github.com/mateeusferro/schedula/internal/repository"
 )
 
@@ -10,4 +11,40 @@ type TaskUseCase struct {
 
 func NewTaskUseCase(repo *repository.TaskRepository) *TaskUseCase {
 	return &TaskUseCase{repo: repo}
+}
+
+func (usecase *TaskUseCase) ExecuteGetTask(id string) (*domain.Task, error) {
+	result, err := usecase.repo.GetTaskInfo(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, err
+}
+
+func (usecase *TaskUseCase) ExecuteGetTasksByStatus(status string) ([]domain.Task, error) {
+	result, err := usecase.repo.GetTasksByStatus(status)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, err
+}
+
+func (usecase *TaskUseCase) ExecuteCreateTask(task domain.TaskInput) (bool, error) {
+	_, err := usecase.repo.CreateTask(task)
+	if err != nil {
+		return false, err
+	}
+
+	return true, err
+}
+
+func (usecase *TaskUseCase) ExecuteDeleteTask(id string) (bool, error) {
+	_, err := usecase.repo.DeleteTask(id)
+	if err != nil {
+		return false, err
+	}
+
+	return true, err
 }
