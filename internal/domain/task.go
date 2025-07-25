245afc866863f54f7933eeb2dd2ec80b5279a66e
mateus <mateus.03fernandesaraujo@gmail.com>
2025-07-24 21:28:33 -0300

    feat: create task domain for input and output

--- internal/domain/task.go
+++ internal/domain/task.go
@@ -1,1 +1,24 @@
 package domain
+
+import "time"
+
+type Task struct {
+	Id           string                 `json:"id"`
+	Name         string                 `json:"name"`
+	Payload      map[string]interface{} `json:"payload"`
+	Run_at       time.Time              `json:"run_at"`
+	Status       string                 `json:"status"`
+	Attempts     int                    `json:"attempts"`
+	Max_attempts int                    `json:"max_attempts"`
+	Created_at   time.Time              `json:"created_at"`
+	Updated_at   time.Time              `json:"updated_at"`
+}
+
+type TaskInput struct {
+	Name         string                 `json:"name"`
+	Payload      map[string]interface{} `json:"payload"`
+	Run_at       time.Time              `json:"run_at"`
+	Status       string                 `json:"status"`
+	Attempts     int                    `json:"attempts"`
+	Max_attempts int                    `json:"max_attempts"`
+}
