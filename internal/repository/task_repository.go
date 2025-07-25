404eb6342c6e38ecb8aaa6dfed0bf19611e90546
mateus <mateus.03fernandesaraujo@gmail.com>
2025-07-24 21:28:46 -0300

    feat: create repository for manipulate the db

--- internal/repository/task_repository.go
+++ internal/repository/task_repository.go
@@ -1,6 +1,10 @@
 package repository
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/mateeusferro/schedula/internal/domain"
+)
 
 type TaskRepository struct {
 	DB *sql.DB
@@ -8,4 +12,91 @@ type TaskRepository struct {
 
 func NewTaskRepository(db *sql.DB) *TaskRepository {
 	return &TaskRepository{DB: db}
+}
+
+func (repository *TaskRepository) CreateTask(task domain.TaskInput) (bool, error) {
+	_, err := repository.DB.Exec(`
+			INSERT INTO SCHEDULED_TASKS
+				(NAME, PAYLOAD, RUN_AT, STATUS, ATTEMPTS, MAX_ATTEMPTS)
+			VALUES
+				($1, $2, $3, $4, $5, $6)
+		`, task.Name, task.Payload, task.Run_at,
+		task.Status, task.Attempts, task.Max_attempts)
+	if err != nil {
+		return false, err
+	}
+
+	return true, err
+}
+
+func (repository *TaskRepository) GetTaskInfo(id string) (*domain.Task, error) {
+	row := repository.DB.QueryRow(`
+			SELECT * FROM SCHEDULED_TASKS 
+			WHERE ID = ?
+		`, id)
+
+	var task domain.Task
+	err := row.Scan(
+		&task.Attempts,
+		&task.Created_at,
+		&task.Id,
+		&task.Max_attempts,
+		&task.Name,
+		&task.Payload,
+		&task.Run_at,
+		&task.Status,
+		&task.Updated_at,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &task, nil
+}
+
+// Next feature maybe add other fields
+func (repository *TaskRepository) GetTasksByStatus(status string) ([]domain.Task, error) {
+	rows, err := repository.DB.Query(`
+		SELECT * FROM SCHEDULED_TASKS
+		WHERE STATUS = ?
+	`, status)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var tasks []domain.Task
+	for rows.Next() {
+		var task domain.Task
+
+		err := rows.Scan(
+			&task.Attempts,
+			&task.Created_at,
+			&task.Id,
+			&task.Max_attempts,
+			&task.Name,
+			&task.Payload,
+			&task.Run_at,
+			&task.Status,
+			&task.Updated_at,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		tasks = append(tasks, task)
+	}
+
+	return tasks, nil
+}
+
+func (repository *TaskRepository) DeleteTask(id string) (bool, error) {
+	_, err := repository.DB.Exec("DELETE FROM SCHEDULED_TASKS WHERE id = $1", id)
+	if err != nil {
+		return false, err
+	}
+
+	return true, err
 }
